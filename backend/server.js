310c787bf2c0fc4d8637c54478a89366dea348d4
Harsh Kumar <167132722+013harsh@users.noreply.github.com>
2026-04-01 11:25:17 +0530

    fix in server

--- backend/server.js
+++ backend/server.js
@@ -5,18 +5,20 @@ const app = require("./src/app");
 const httpserver = require("http").createServer(app);
 
 const initSocketServer = require("./src/sockets/socket.server");
-
 const connectDB = require("./src/db/db");
-connectDB();
 
+connectDB();
 initSocketServer(httpserver);
 
+// serve frontend
 app.use(require("express").static(path.join(__dirname, "public")));
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// httpserver.listen(3000, () => console.log("server on port 3000"));
-httpserver.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+httpserver.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
