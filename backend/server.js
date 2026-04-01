b0f879ba3c64e2acb9b59680ebf05292e680fe35
Harsh Kumar <167132722+013harsh@users.noreply.github.com>
2026-04-01 11:29:21 +0530

    fix in servera

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,16 @@
 require("dotenv").config();
-const path = require("path");
 
 const app = require("./src/app");
 const httpserver = require("http").createServer(app);
 
 const initSocketServer = require("./src/sockets/socket.server");
-const connectDB = require("./src/db/db");
 
+const connectDB = require("./src/db/db");
 connectDB();
+
 initSocketServer(httpserver);
 
-// serve frontend
-app.use(require("express").static(path.join(__dirname, "public")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-const PORT = process.env.PORT || 3000;
-
-httpserver.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+// httpserver.listen(3000, () => console.log("server on port 3000"));
+httpserver.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
