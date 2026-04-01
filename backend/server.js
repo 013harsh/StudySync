90a919073fac91b09934baf1d4ea788cdd795df0
Harsh Kumar <167132722+013harsh@users.noreply.github.com>
2026-04-01 11:17:29 +0530

    fix in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
+const path = require("path");
+
 const app = require("./src/app");
 const httpserver = require("http").createServer(app);
+
 const initSocketServer = require("./src/sockets/socket.server");
 
 const connectDB = require("./src/db/db");
@@ -8,9 +11,9 @@ connectDB();
 
 initSocketServer(httpserver);
 
-app.use(require("express").static(path.join(__dirname, "dist")));
+app.use(require("express").static(path.join(__dirname, "public")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // httpserver.listen(3000, () => console.log("server on port 3000"));
