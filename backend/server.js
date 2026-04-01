5add59c7bec74c005d2b45d8de7308a43737675e
Harsh Kumar <167132722+013harsh@users.noreply.github.com>
2026-04-01 11:12:26 +0530

    add in server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,12 @@ connectDB();
 
 initSocketServer(httpserver);
 
+app.use(require("express").static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 // httpserver.listen(3000, () => console.log("server on port 3000"));
-httpserver.listen(3000, "0.0.0.0", () => {
+httpserver.listen(3000, () => {
   console.log("Server running on port 3000");
 });
