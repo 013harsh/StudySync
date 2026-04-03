38c7c8f7cd956ab781b633fe085afdfb22d8d0e2
Harsh Kumar <167132722+013harsh@users.noreply.github.com>
2026-04-03 14:30:02 +0530

    update

--- frontend/src/store/store.jsx
+++ frontend/src/store/store.jsx
@@ -2,13 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducer/auth.slice";
 import chatReducer from "./reducer/chat.slice";
 import groupReducer from "./reducer/group.slice";
-import notesReducer from "./reducer/notes.slice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     chat: chatReducer,
     group: groupReducer,
-    notes: notesReducer,
   },
 });
